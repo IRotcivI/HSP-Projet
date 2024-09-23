5f401800fb71ae673ba587d8425fe35e1a1208ba
GUILLON <n.guillon@lprs.fr>
2024-09-23 17:04:25 +0200

    Création de la table hopital Laravel

---
+++ HSP-App/database/migrations/2024_09_23_145917_hopital.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('hopital', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('Nom');
+            $table->string('Specialite');
+            $table->string('Rue');
+            $table->string('Voie');
+            $table->integer('CP');
+            $table->integer('ref_utilisateur');
+            $table->timestamps();
+        });
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
