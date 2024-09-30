d4dd72ccee7e134667b254e742264eb489e55d55
Ahad <147802832+ahad93140@users.noreply.github.com>
2024-09-30 15:57:44 +0200

    Fonction DoLogin

--- HSP-App/app/Http/Controllers/AuthController.php
+++ HSP-App/app/Http/Controllers/AuthController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
+
 class AuthController
 {
 public function login(){
     return_view('auth.login');
 }
-public function doLogin(){
+public function doLogin(LoginRequest $request){
 
 }
 }
