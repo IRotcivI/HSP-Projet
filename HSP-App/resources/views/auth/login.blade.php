fd8779a24cb8618b77a1f0bd05c19ffadf7a93b5
Ahad <147802832+ahad93140@users.noreply.github.com>
2024-09-30 16:47:46 +0200

    Insertion des options email et mdp

--- HSP-App/resources/views/auth/login.blade.php
+++ HSP-App/resources/views/auth/login.blade.php
@@ -1,11 +1,30 @@
-<?php
-@extends ("base")
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
 
-@section("content")
+    <h1>Connexion</h1>
+    <div class="row text-center py-3 mt-3">
+        <div class="col-4 mx-auto">
+            <input type="text" placeholder="Email" class="form-control" >
+        </div>
+    </div>
+    <div class="row text-center py-3 mt-3">
+        <div class="col-4 mx-auto">
+            <input type="text" placeholder="Mot de Passe" class="form-control" >
+        </div>
+    </div>
 
-    <h1> Se connecter </h1>
-<div class="card">
-    <div class="card-body">
-    <form action="{{ route('auth.login') }}"method="post"class="vstack gap-3">
-
-@endsection
+    <div class="row text-center py-3 mt-3">
+            <button type="button" class="btn btn-success w-auto me-1 mb-0">Success</button>
+        </div>
+    </div>
+    </div>
+</body>
+</html>
