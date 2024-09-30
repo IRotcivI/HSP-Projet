e20921775e62fbb9df959db8c7e4ea888a6be126
Ahad <147802832+ahad93140@users.noreply.github.com>
2024-09-30 17:07:08 +0200

    Modification pour associer avec le css

--- HSP-App/resources/views/auth/login.blade.php
+++ HSP-App/resources/views/auth/login.blade.php
@@ -2,8 +2,13 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport">
+    <head>
+        <meta charset="utf-8">
+        <title>Ma page</title>
+        <link href="/HSP-App/resources/css/loginn.css" rel="stylesheet">
+    </head>
+
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
