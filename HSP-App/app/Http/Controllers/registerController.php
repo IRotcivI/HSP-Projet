587412744c428f3c615defa6ddc62d1960b52f4b
GUILLON <n.guillon@lprs.fr>
2024-10-07 16:19:13 +0200

    Mise à jour de la page d'inscription

--- HSP-App/app/Http/Controllers/registerController.php
+++ HSP-App/app/Http/Controllers/registerController.php
@@ -2,26 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class registerController extends Controller
 {
+
     public function store (Request $request)
     {
         $request-> validate([
-            'name'=>'required',
+            'nom'=>'required',
+            'prenom'=>'required',
             'email'=>'required|email|unique:users',
             'password'=>'required|confirmed|min:8',
         ]);
-        $user=User::create([
-            'name'=>$request->name,
-            'email'=>$request->email,
-            'password'=>Hash::make($request->password),
+        $utilisateur=utilisateur::create([
+            'nom' => $request->nom,
+            'prenom' => $request->prenom,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
         ]);
         return back ();
     }
     public function create()
     {
-        return view('register.blade');
+        return view('auth.register');
     }
+
 }
