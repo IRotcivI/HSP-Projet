5f92f6dcc74540c1f71618fcf744c175df76cd8a
DJAIZ_Se <selimdjaiz@gmail.com>
2024-10-07 16:34:41 +0200

    Ajout de la page html de la fiche entreprise

---
+++ HSP-App/resources/views/profil/profilEntreprise.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Fiche Entreprise</title>
+</head>
+<body>
+<tr>
+    <th>
+        <h2>Fiche Entreprise</h2>
+    </th>
+</tr>
+<tr>
+    <th>
+        Nom entreprise :
+    <br><br>
+    </th>
+    <th>
+        Adresse :
+    <br><br>
+    </th>
+    <th>
+        Site web :
+    <br><br>
+    </th>
+</tr>
+</body>
+</html>
+
