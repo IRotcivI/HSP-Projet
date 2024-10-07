aefd6c42d9e25dd41500e17d6362c51938194886
DJAIZ_Se <selimdjaiz@gmail.com>
2024-10-07 16:45:15 +0200

    Ajout de la page html de la fiche Utilisateur

--- HSP-App/resources/views/profil/profilUtilisateur.blade.php
+++ HSP-App/resources/views/profil/profilUtilisateur.blade.php
@@ -1,1 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Fiche Utilisateur</title>
+</head>
+<body>
+<tr>
+    <th>
+        <h2>Fiche Utilisateur</h2>
+    </th>
+</tr>
+<tr>
+    <th>
+        Nom :
+        <br><br>
+    </th>
+    <th>
+        Prenom :
+        <br><br>
+    </th>
+    <th>
+        Email :
+        <br><br>
+    </th>
+    <th>
+        Fonction :
+        <br><br>
+    </th>
+    <th>
+        CV :
+        <br><br>
+    </th>
+    <th>
+        Entreprise :
+        <br><br>
+    </th>
+    <th>
+        Mot de passe :
+        <br><br>
+    </th>
+
+</tr>
+</body>
+</html>
+
 
