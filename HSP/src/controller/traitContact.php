a00ff3e4acf12907737288377deef873cc4c1bc6
ahad93140 <r.asad@lprs.fr>
2024-12-04 12:32:56 +0100

    Lien avec contact.php

--- HSP/src/controller/traitContact.php
+++ HSP/src/controller/traitContact.php
@@ -1,1 +1,43 @@
 <?php
+
+if (isset($_POST['submit'])) {
+    include '../database/Bdd.php';
+    include '../model/Utilisateur.php';
+    include '../vue/contact.php';
+
+    if (empty($_POST['nom']) || empty($_POST['prenom']) || empty($_POST['email']) || empty($_POST['mdp']) || empty($_POST['mdp2']) || empty($_POST['specialite'])) {
+        header("Location:/HSP/vue/auth/entreprise/formRegisterEntreprise.php?inscription=vide");
+        exit();
+    }
+    else {
+        if (!preg_match("/^[a-zA-Z ]*$/",$_POST['nom']) || !preg_match("/^[a-zA-Z ]*$/",$_POST['prenom'])) {
+            header("Location:/HSP/vue/auth/entreprise/formRegisterEntreprise.php?inscription=caractere");
+            exit();
+        }
+        else{
+            if ($_POST['mdp'] != $_POST['mdp2']) {
+                header("Location:/HSP/vue/auth/entreprise/formRegisterEntreprise.php?inscription=passwordincorect");
+            }
+            else {
+                if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+                    header("Location:/HSP/vue/auth/entreprise/formRegisterEntreprise.php?inscription=emailinvalide");
+                    exit();
+                }
+                else {
+                    $ins = new Contact([
+                        'email' => $_POST['email'],
+                        'motif' => $_POST['motif'],
+                        'demande' => $_POST['demande'],
+                        'autre' => $_POST['autre'],
+                    ]);
+                    $ins->Contact();
+                }
+            }
+        }
+
+    }
+}
+else {
+    header("Location:/HSP/vue/auth/eleve/formRegisterEleve.php?inscription=erreur");
+    exit();
+}
